25f23744129d75f6f28eaff803d90842b02b683c
ananya-research <ananya.303.singh@gmail.com>
2024-12-27 21:16:11 -0700

    Time: 7 ms (88.99%) | Memory: 18 MB (9.77%) - LeetSync

--- 372-super-pow/super-pow.py
+++ 372-super-pow/super-pow.py
@@ -2,25 +2,27 @@ class Solution:
     def superPow(self, a: int, b: List[int]) -> int:
 
         
-        p="".join(map(str, b))
-        p=int(p)
-        print(p)
+        # p="".join(map(str, b))
+        # p=int(p)
+        # print(p)
         
-        def helper(a,p):
+        # def helper(a,p):
 
-            a=a%1337
+        #     a=a%1337
 
-            if a==0:
-                return 0
+        #     if a==0:
+        #         return 0
 
-            if p==0:
-                return 1
+        #     if p==0:
+        #         return 1
             
             
 
-            res=helper(a*a,p//2)%1337
-            return (a*res)%1337 if p%2 else res%1337
+        #     res=helper(a*a,p//2)%1337
+        #     return (a*res)%1337 if p%2 else res%1337
 
-        res=helper(a,p)%1337
-        return res
+        # res=helper(a,p)%1337
+        # return res
             
+        p=int("".join(map(str,b)))
+        return pow(a,p,1337)
