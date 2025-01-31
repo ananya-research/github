30ed3ae94dbd8009772d6870adf64c4b65fc45b7
ananya-research <ananya.303.singh@gmail.com>
2025-01-30 17:41:30 -0700

    Time: 0 ms (100.00%) | Memory: 18.6 MB (63.00%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -3,9 +3,30 @@ class Solution:
 
         n=len(nums)
 
-        for i in range(n):
-            if sum(nums[:i])==sum(nums[i+1:]):
+        # for i in range(n):
+        #     if sum(nums[:i])==sum(nums[i+1:]):
+        #         return i
+
+        # return -1
+
+        left_sum=0
+
+        right_sum=sum(nums[1:])
+
+        if right_sum==left_sum:
+                return 0
+
+   
+        
+        
+        for i in range(1,n):
+
+            left_sum+=nums[i-1]
+            right_sum-=nums[i]
+
+            if right_sum==left_sum:
                 return i
 
         return -1
-        
+            
+
