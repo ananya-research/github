fd21f4a1c21c1bfcb2268f4c05d9a7468bf5a886
ananya-research <ananya.303.singh@gmail.com>
2025-03-03 22:35:41 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (91.41%) - LeetSync

--- 974-reorder-data-in-log-files/reorder-data-in-log-files.py
+++ 974-reorder-data-in-log-files/reorder-data-in-log-files.py
@@ -12,10 +12,14 @@ class Solution:
         
         for i, log in enumerate(logs):
             vals=log.split(" ")
+            
             if hasNum(vals[1:]):
                 digit_log.append(log)
             else:
                 letter_log.append([' '.join(vals[1:]), vals[0], i])
+
+        print(letter_log)
+        
                 
         for log in sorted(letter_log):
             res.append(logs[log[2]])
