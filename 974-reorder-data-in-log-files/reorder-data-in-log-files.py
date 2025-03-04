d5cae3329d68a734d0dbf6d3fc78134b8124e73a
ananya-research <ananya.303.singh@gmail.com>
2025-03-03 22:31:12 -0700

    Time: 1 ms (36.30%) | Memory: 18 MB (23.22%) - LeetSync

---
+++ 974-reorder-data-in-log-files/reorder-data-in-log-files.py
@@ -0,0 +1,26 @@
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        
+        res=[]
+        
+        
+        digit_log, letter_log=[], []
+        
+        def hasNum(inp):
+            return any(char.isdigit() for char in inp)
+        
+        
+        for i, log in enumerate(logs):
+            vals=log.split(" ")
+            if hasNum(vals[1:]):
+                digit_log.append(log)
+            else:
+                letter_log.append([' '.join(vals[1:]), vals[0], i])
+                
+        for log in sorted(letter_log):
+            res.append(logs[log[2]])
+            
+        
+        
+        return res+digit_log
+        
