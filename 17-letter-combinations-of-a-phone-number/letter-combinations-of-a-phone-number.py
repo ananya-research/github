0f3f7007eb0ea1734e9f6ae78d074a90e0b32c7a
ananya-research <ananya.303.singh@gmail.com>
2025-04-06 16:44:05 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (23.34%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,39 +1,96 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        
-        ans=[]  
+        res=[]
         sol=[]
-        n=len(digits)
 
         if digits=="":
             return []
-        hmap={'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9':'wxyz'}
-        
-        def backtrack(i=0):
-            if i==n:
-                ans.append("".join(sol))
-                return
-            for letter in hmap[digits[i]]:
 
-                sol.append(letter)
-                backtrack(i+1)
-                sol.pop()
+        n=len(digits)
+        hmap={'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9':'wxyz'}
+
+        def backtrack(i):
+
+            if i==n:
+                res.append("".join(sol))
+                return
+            else:
+                for letter in hmap[digits[i]]:
+                    sol.append(letter)
+                    backtrack(i+1)
+                    sol.pop()
 
         backtrack(0)
-        return ans
-    
-#     def backtrack(i):
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
-#         if i==n:
-#             ans.append("".join(sol))
-#             return
+        # ans=[]  
+        # sol=[]
+        # n=len(digits)
+
+        # if digits=="":
+        #     return []
+
+        # hmap={'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9':'wxyz'}
         
-#         for letter in hmap[digits[i]]:
-            
-#             sol.append(letter)
-#             backtrack(i+1)
-#             sol.pop()
-            
+        # def backtrack(i=0):
+        #     if i==n:
+        #         ans.append("".join(sol))
+        #         return
+        #     for letter in hmap[digits[i]]:
+
+        #         sol.append(letter)
+        #         backtrack(i+1)
+        #         sol.pop()
+
+        # backtrack(0)
+        # return ans
+
             
         
         
