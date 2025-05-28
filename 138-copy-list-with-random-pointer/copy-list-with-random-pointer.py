c3dcf2f305f9fd39a965124748903cbd4fc46df2
ananya-research <ananya.303.singh@gmail.com>
2025-05-28 14:21:00 -0700

    Time: 45 ms (30.25%) | Memory: 18.5 MB (59.41%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -9,28 +9,46 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        
+        # if not head: return None
 
-        if not head: return None
+        # cur=head
+        # old_to_new={}
 
-        cur=head
+        # while cur:
+        #     node=Node(x=cur.val)
+        #     old_to_new[cur]=node
+        #     cur=cur.next
+
+        # cur=head
+
+        # while cur:
+        #     new_node=old_to_new[cur]
+        #     new_node.next=old_to_new[cur.next] if cur.next else None
+        #     new_node.random=old_to_new[cur.random] if cur.random else None
+        #     cur=cur.next
+        
+        # return old_to_new[head]
+
+
+
+        if not head:
+            return None
+        
+        curr=head
         old_to_new={}
 
-        while cur:
-            node=Node(x=cur.val)
-            old_to_new[cur]=node
-            cur=cur.next
-
-        cur=head
-
-        while cur:
-            new_node=old_to_new[cur]
-            new_node.next=old_to_new[cur.next] if cur.next else None
-            new_node.random=old_to_new[cur.random] if cur.random else None
-            cur=cur.next
+        while curr:
+            node=Node(x=curr.val)
+            old_to_new[curr]=node
+            curr=curr.next
+        
+        curr=head
+        while curr:
+            node=old_to_new[curr]
+            node.next=old_to_new[curr.next] if curr.next else None
+            node.random=old_to_new[curr.random] if curr.random else None
+            curr=curr.next
         
         return old_to_new[head]
-
-        
-
-
         
