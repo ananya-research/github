ae4eb14e39e1a91b8ae65d46270e6cc3efccea86
ananya-research <ananya.303.singh@gmail.com>
2025-05-27 18:04:09 -0700

    Time: 229 ms (92.17%) | Memory: 20.3 MB (58.34%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -54,8 +54,40 @@ class Solution:
                 
                     
         # return num_of_islands
+
+
+
+
                     
 
+
+
+
+
+        # n=len(grid)
+        # m=len(grid[0])
+
+        # num=0
+
+        # def dfs(i,j):
+
+        #     if i<0 or i>=n or j<0 or j>=m or grid[i][j]!='1':
+        #         return
+        #     else:
+        #         grid[i][j]='0'
+        #         dfs(i+1,j)
+        #         dfs(i-1,j)
+        #         dfs(i,j+1)
+        #         dfs(i,j-1)
+
+
+        # for i in range(n):
+        #     for j in range(m):
+        #         if grid[i][j]=='1':
+        #             dfs(i,j)
+        #             num+=1
+        
+        # return num
 
 
 
@@ -63,29 +95,27 @@ class Solution:
         n=len(grid)
         m=len(grid[0])
 
-        num=0
+        num_of_islands=0
 
         def dfs(i,j):
-
             if i<0 or i>=n or j<0 or j>=m or grid[i][j]!='1':
-                return
+                return 
             else:
                 grid[i][j]='0'
-                dfs(i+1,j)
-                dfs(i-1,j)
                 dfs(i,j+1)
                 dfs(i,j-1)
+                dfs(i+1,j)
+                dfs(i-1,j)
+
 
 
         for i in range(n):
             for j in range(m):
                 if grid[i][j]=='1':
+                    num_of_islands+=1
                     dfs(i,j)
-                    num+=1
-        
-        return num
 
-
+        return num_of_islands
 
 
 
