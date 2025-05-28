6d3536a9aa8342cdf4ee94dcc6d8e6ec0b4c7018
ananya-research <ananya.303.singh@gmail.com>
2025-05-28 16:16:31 -0700

    Time: 4 ms (48.59%) | Memory: 17.8 MB (45.02%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -27,43 +27,84 @@ class Solution:
 
 
 
-        rows=len(grid)
-        cols=len(grid[0])
+        # rows=len(grid)
+        # cols=len(grid[0])
 
-        num_fresh=0
+        # num_fresh=0
 
-        q=deque()
+        # q=deque()
 
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j]==2:
-                    q.append((i,j))
-                elif grid[i][j]==1:
-                    num_fresh+=1
+        # for i in range(rows):
+        #     for j in range(cols):
+        #         if grid[i][j]==2:
+        #             q.append((i,j))
+        #         elif grid[i][j]==1:
+        #             num_fresh+=1
 
-        if num_fresh==0:
+        # if num_fresh==0:
+        #     return 0
+
+        
+        # minutes=-1
+
+        # while q:
+        #     q_size=len(q)
+        #     minutes+=1
+
+        #     for _ in range(q_size):
+        #         i,j=q.popleft()
+
+        #         for r,c in [(i+1,j),(i,j+1),(i-1,j),(i,j-1)]:
+        #             if 0<=r<rows and 0<=c<cols and grid[r][c]==1:
+        #                 grid[r][c]=2
+        #                 num_fresh-=1
+        #                 q.append((r,c))
+        
+        # if num_fresh==0:
+        #     return minutes
+        # else:
+        #     return -1
+            
+
+
+
+        numFresh=0
+        queue=deque()
+        m=len(grid)
+        n=len(grid[0])
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==1:
+                    numFresh+=1
+                elif grid[i][j]==2:
+                    queue.append((i,j))
+
+        if numFresh==0:
             return 0
 
-        
-        minutes=-1
 
-        while q:
-            q_size=len(q)
-            minutes+=1
+        mins=-1
 
-            for _ in range(q_size):
-                i,j=q.popleft()
-
-                for r,c in [(i+1,j),(i,j+1),(i-1,j),(i,j-1)]:
-                    if 0<=r<rows and 0<=c<cols and grid[r][c]==1:
+        while queue:
+            q=len(queue)
+            mins+=1
+            for _ in range(q):
+                i,j=queue.popleft()
+                for r,c in [(i,j+1), (i,j-1), (i+1,j), (i-1,j)]:
+                    if 0<=r<m and 0<=c<n and grid[r][c]==1:
                         grid[r][c]=2
-                        num_fresh-=1
-                        q.append((r,c))
+                        numFresh-=1
+                        queue.append((r,c))
+
+            
         
-        if num_fresh==0:
-            return minutes
+        if numFresh==0:
+            return mins
+        
         else:
             return -1
+
 
 
 
