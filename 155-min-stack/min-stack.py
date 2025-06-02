15c3a8668dc9364e27e272bb72ce93cc304786ed
ananya-research <ananya.303.singh@gmail.com>
2025-06-01 17:34:50 -0700

    Time: 15 ms (13.37%) | Memory: 21.3 MB (63.59%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -2,14 +2,22 @@ class MinStack:
 
     def __init__(self):
         self.stack=[]
+        self.min_stack = []
         
 
     def push(self, val: int) -> None:
         self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+        else:
+            self.min_stack.append(self.min_stack[-1])
         
 
     def pop(self) -> None:
+        
+
         self.stack.pop()
+        self.min_stack.pop()
         
 
     def top(self) -> int:
@@ -17,7 +25,7 @@ class MinStack:
         
 
     def getMin(self) -> int:
-        return min(self.stack)
+        return self.min_stack[-1]
         
 
 
