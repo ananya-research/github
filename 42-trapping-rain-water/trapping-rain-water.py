9d118f3ee77eddd315450d4ea99cba4eb53228d9
ananya-research <ananya.303.singh@gmail.com>
2025-06-03 21:21:29 -0700

    Time: 11 ms (73.68%) | Memory: 19.4 MB (19.50%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -42,47 +42,47 @@ class Solution:
 
         # return summ
 
-        # n=len(height)
-        # l=0
-        # r=n-1
-        # max_left=height[l]
-        # max_right=height[r]
+        n=len(height)
+        l=0
+        r=n-1
+        max_left=height[l]
+        max_right=height[r]
 
-        # summ=0
+        summ=0
 
-        # while l<r:
-        #     if max_left<max_right:
-        #         l+=1
-        #         max_left=max(max_left, height[l])
-        #         summ+=max_left-height[l]
+        while l<r:
+            if max_left<max_right:
+                l+=1
+                max_left=max(max_left, height[l])
+                summ+=max_left-height[l]
 
-        #     else:
-        #         r-=1
-        #         max_right=max(max_right, height[r])
-        #         summ+=max_right-height[r]
+            else:
+                r-=1
+                max_right=max(max_right, height[r])
+                summ+=max_right-height[r]
 
-        # return summ
+        return summ
 
-        n = len(height)
-        if n == 0:
-            return 0
+        # n = len(height)
+        # if n == 0:
+        #     return 0
 
-        left_max = [0] * n
-        right_max = [0] * n
+        # left_max = [0] * n
+        # right_max = [0] * n
 
-        left_max[0] = height[0]
-        for i in range(1, n):
-            left_max[i] = max(left_max[i - 1], height[i])
+        # left_max[0] = height[0]
+        # for i in range(1, n):
+        #     left_max[i] = max(left_max[i - 1], height[i])
 
-        right_max[-1] = height[-1]
-        for i in range(n - 2, -1, -1):
-            right_max[i] = max(right_max[i + 1], height[i])
+        # right_max[-1] = height[-1]
+        # for i in range(n - 2, -1, -1):
+        #     right_max[i] = max(right_max[i + 1], height[i])
 
-        total_water = 0
-        for i in range(n):
-            total_water += min(left_max[i], right_max[i]) - height[i]
+        # total_water = 0
+        # for i in range(n):
+        #     total_water += min(left_max[i], right_max[i]) - height[i]
 
-        return total_water
+        # return total_water
 
 
         
