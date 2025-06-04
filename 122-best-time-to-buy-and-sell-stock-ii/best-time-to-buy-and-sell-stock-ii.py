d5e5d3b71814f5850014d0c9e0a6b8a8e0d50314
ananya-research <ananya.303.singh@gmail.com>
2025-06-03 20:52:45 -0700

    Time: 0 ms (100.00%) | Memory: 18.8 MB (69.54%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -8,15 +8,41 @@ class Solution:
         
         # return maxProf
 
-        profit=0
+        # profit=0
 
-        for i in range(1,len(prices)):
-            if prices[i]>prices[i-1]:
-                profit+=prices[i]-prices[i-1]
+        # for i in range(1,len(prices)):
+        #     if prices[i]>prices[i-1]:
+        #         profit+=prices[i]-prices[i-1]
 
+        # return profit
+
+        # lo=prices[0]
+        # hi=prices[0]
+        # profit=0
+        # i=0
+        # n=len(prices)
+
+        # while i<n-1:
+
+        #     while i<n-1 and prices[i]>=prices[i+1]:
+        #         i+=1
+        #     lo=prices[i]
+
+        #     while i<n-1 and prices[i]<prices[i+1]:
+        #         i+=1
+        #     hi=prices[i]
+
+        #     profit+=hi-lo
+        # return profit
+
+
+        profit = 0
+        for i in range(1, len(prices)):
+            # If today's price is higher than yesterday's, take the profit
+            if prices[i] > prices[i - 1]:
+                profit += prices[i] - prices[i - 1]
         return profit
-            
 
-        
 
-        
+
+
