5434f288726246df26d3eb8fef9da573bbeaa7bf
ananya-research <ananya.303.singh@gmail.com>
2025-09-07 20:30:45 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (38.51%) - LeetSync

--- 68-text-justification/text-justification.py
+++ 68-text-justification/text-justification.py
@@ -3,30 +3,77 @@ class Solution:
         n=len(words)
         length=0
         line=[]
-        result=[]
         i=0
+        result=[]
 
         while i<n:
             if len(line)+length+len(words[i])>maxWidth:
                 extra_spaces=maxWidth-length
-                spaces=extra_spaces//max(1, len(line)-1)
-                remainder=extra_spaces%max(1,len(line)-1)
-                for j in range( max (1, len(line)-1 ) ):
-                    line[j]+=" "*spaces
-                    if remainder:
+                space_to_add=extra_spaces//max(1,len(line)-1)
+                remaining_spaces=extra_spaces%max(1,len(line)-1)
+                for j in range(max(1,len(line)-1)):
+                    line[j]+=" "*space_to_add
+                    if remaining_spaces:
                         line[j]+=" "
-                        remainder-=1
+                        remaining_spaces-=1
                 result.append("".join(line))
-                line, length=[], 0
+                length=0
+                line=[]
             line.append(words[i])
             length+=len(words[i])
             i+=1
-
+        
         last_line=" ".join(line)
         trailing_spaces=maxWidth-len(last_line)
         last_line+=" "*trailing_spaces
         result.append(last_line)
         return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # n=len(words)
+        # length=0
+        # line=[]
+        # result=[]
+        # i=0
+
+        # while i<n:
+        #     if len(line)+length+len(words[i])>maxWidth:
+        #         extra_spaces=maxWidth-length
+        #         spaces=extra_spaces//max(1, len(line)-1)
+        #         remainder=extra_spaces%max(1,len(line)-1)
+        #         for j in range( max (1, len(line)-1 ) ):
+        #             line[j]+=" "*spaces
+        #             if remainder:
+        #                 line[j]+=" "
+        #                 remainder-=1
+        #         result.append("".join(line))
+        #         line, length=[], 0
+        #     line.append(words[i])
+        #     length+=len(words[i])
+        #     i+=1
+
+        # last_line=" ".join(line)
+        # trailing_spaces=maxWidth-len(last_line)
+        # last_line+=" "*trailing_spaces
+        # result.append(last_line)
+        # return result
 
                     
         
