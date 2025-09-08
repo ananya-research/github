26ebb5803a32b7c94f0c7f6317dcc1941eaa7598
ananya-research <ananya.303.singh@gmail.com>
2025-09-07 22:02:34 -0400

    Time: 0 ms (100.00%) | Memory: 19.1 MB (62.33%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,37 +1,142 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        
-        
-        g = defaultdict(list)
-        courses = prerequisites
-        for a, b in courses:
-            g[a].append(b)
 
-        UNVISITED = 0
-        VISITING = 1
-        VISITED = 2
-        states = [UNVISITED] * numCourses
+        graph=defaultdict(list)
+
+        for a,b in prerequisites:
+            graph[a].append(b)
+
+        UNVISITED=0
+        VISITING=1
+        VISITED=2
+
+        states=[UNVISITED]*numCourses
 
         def dfs(node):
-            state = states[node]
-            if state == VISITED:
-                return True  # Already processed; no need to visit again
-            elif state == VISITING:
-                return False  # Found a cycle
 
-            states[node] = VISITING  # Mark the node as being visited
-
-            for nei in g[node]:  # Explore all neighbors (prerequisites)
-                if not dfs(nei):  # If a cycle is detected in any neighbor, return False
-                    return False
-
-            states[node] = VISITED  # Mark the node as fully processed
-            return True
-        
-        for i in range(numCourses):
-            if not dfs(i):  # If a cycle is found for any course, return False
+            state=states[node]
+            if state==VISITED:
+                return True
+            
+            elif state==VISITING:
                 return False
+            
+            states[node]=VISITING
+
+            for nei in graph[node]:
+                if not dfs(nei):
+                    return False
+                
+            states[node]=VISITED
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        
         return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        # graph=defaultdict(list)
+        
+        # for a,b in prerequisites:
+        #     graph[a].append(b)
+            
+        # UNVISITED=0
+        # VISITING=1
+        # VISITED=2
+        # states=[UNVISITED]*numCourses
+        
+        # def dfs(node):
+        #     state=states[node]
+            
+        #     if state==VISITED:
+        #         return True
+            
+        #     elif state==VISITING:
+        #         return False
+            
+        #     states[node]=VISITING
+            
+        #     for nei in graph[node]:
+        #         if not dfs(nei):
+        #             return False
+            
+        #     states[node]=VISITED
+        #     return True
+        
+        # for i in range(numCourses):
+        #     if not dfs(i):
+        #         return False
+        
+        
+        # return True
+        
+        
+                
+
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         g = defaultdict(list)
+#         courses = prerequisites
+#         for a, b in courses:
+#             g[a].append(b)
+
+#         UNVISITED = 0
+#         VISITING = 1
+#         VISITED = 2
+#         states = [UNVISITED] * numCourses
+
+#         def dfs(node):
+#             state = states[node]
+#             if state == VISITED:
+#                 return True  # Already processed; no need to visit again
+#             elif state == VISITING:
+#                 return False  # Found a cycle
+
+#             states[node] = VISITING  # Mark the node as being visited
+
+#             for nei in g[node]:  # Explore all neighbors (prerequisites)
+#                 if not dfs(nei):  # If a cycle is detected in any neighbor, return False
+#                     return False
+
+#             states[node] = VISITED  # Mark the node as fully processed
+#             return True
+        
+#         for i in range(numCourses):
+#             if not dfs(i):  # If a cycle is found for any course, return False
+#                 return False
+#         return True
 
 
                 
